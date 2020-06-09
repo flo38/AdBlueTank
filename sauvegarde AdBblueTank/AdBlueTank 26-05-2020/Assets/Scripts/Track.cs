247a059559d4c88b5347e6b23980ca49f2ce62e5
flo-flo007 <coulet.florian38@gmail.com>
2020-06-09 23:22:46 +0200

    premier depot

---
+++ sauvegarde AdBblueTank/AdBlueTank 26-05-2020/Assets/Scripts/Track.cs
@@ -0,0 +1,15 @@
+﻿/* .-------.                             .--.    .-------.     .--.            .--.     .--.        
+   |       |--.--.--------.-----.-----.--|  |    |_     _|--.--|  |_.-----.----|__|---.-|  |-----.
+   |   -   |_   _|        |  _  |     |  _  |      |   | |  |  |   _|  _  |   _|  |  _  |  |__ --|
+   |_______|__.__|__|__|__|_____|__|__|_____|      |___| |_____|____|_____|__| |__|___._|__|_____|
+   © OXMOND / www.oxmond.com */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour {
+
+    public Animator animator;
+
+}
